d95471c929f850f87b552b09e46ce6df8040d849
Eva Kane <betaxab@gmail.com>
2019-04-05 01:00:16 +0800

    beckham: update Pie blobs from PPW29.131-27-1
    Signed-off-by: Eva Kane <betaxab@gmail.com>

--- recovery/root/sbin/prepdecrypt.sh
+++ recovery/root/sbin/prepdecrypt.sh
@@ -82,6 +82,7 @@ cp /v/lib64/libtime_genoff.so /vendor/lib64/
 cp /v/lib64/libkeymasterdeviceutils.so /vendor/lib64/
 cp /v/lib64/libkeymasterprovision.so /vendor/lib64/
 cp /v/lib64/libkeymasterutils.so /vendor/lib64/
+cp /v/lib64/libqtikeymaster4.so /vendor/lib64/
 cp /v/lib64/hw/bootctrl.sdm660.so /vendor/lib64/hw/
 cp /v/lib64/hw/gatekeeper.sdm660.so /vendor/lib64/hw/
 cp /v/lib64/hw/keystore.sdm660.so /vendor/lib64/hw/
