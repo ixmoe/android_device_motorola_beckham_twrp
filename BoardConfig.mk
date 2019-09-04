a48f52fa916d32d13b7b9afa514d01683ab5b64d
Beta Soft <betaxab@gmail.com>
2019-09-05 06:02:01 +0800

    beckham: enable MTP and include repack tools
    Signed-off-by: Beta Soft <betaxab@gmail.com>
    Change-Id: I7e50eec525b6f6fde6e918bf222776dd4c53e54d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,8 +109,9 @@ TW_EXCLUDE_DEFAULT_USB_INIT := true
 TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
 TW_USE_TOOLBOX := true
 TW_EXCLUDE_TWRPAPP := true
-# MTP will not work until we update it to support ffs
-TW_EXCLUDE_MTP := true
+
+# Installer
+TW_INCLUDE_REPACKTOOLS := true
 
 # Asian region languages
 TW_EXTRA_LANGUAGES := true
